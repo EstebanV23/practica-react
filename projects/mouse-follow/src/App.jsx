0531c1b37133cd5292dbf5443d4b6f65ec44ba25
Brayan Villamizar <brayan.villa.fer@gmail.com>
2023-02-21 22:19:07 -0500

    Finish the mouse follow app

--- projects/mouse-follow/src/App.jsx
+++ projects/mouse-follow/src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App () {
-  const [count, setCount] = useState(0)
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    enabled && window.addEventListener('pointermove', handleMove) // Si esta en enabled se ejecutará el eventListener
+
+    // Se ejecuta al principio, cuando se monta o desmonta el componente, y al cambiar su dependencia que en este caso es el enabled
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
 
   return (
-    <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgb(0, 0, 0, 0.5)',
+        border: '1px solid white',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <h1>Mouse Follow</h1>
+      <button onClick={() => setEnabled(!enabled)}>{enabled ? 'Desactivar' : 'Activar'} Seguir puntero</button>
+    </>
   )
 }
 
